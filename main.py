c09a6620fa8041595c5365c6fe04d80cd77bf143
Michal <michalgrzegorczyk04@gmail.com>
2026-02-01 18:18:55 +0100

    feat: generating prompts dynamically with dynamic content

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import os
+import json
 
 from openai import OpenAI
 from dotenv import load_dotenv
@@ -10,6 +11,23 @@ client = OpenAI(
 )
 
 def generate_x_post(topic: str) -> str:
+    with open("post-examples.json", "r") as f:
+        examples = json.load(f)
+
+    examples_str = ""
+    for i, example in enumerate(examples, 1):
+        examples_str += f"""
+        <example-{i}>
+            <topic>
+            {example['topic']}
+            </topic>
+
+            <generated-post>
+            {example['post']}
+            </generated-post>
+        </example-{i}>
+        """
+
     prompt = f"""
         You are an expert social media manager, and you excel at crafting viral and highly engaging posts for X (formerly Twitter).
 
@@ -20,8 +38,16 @@ def generate_x_post(topic: str) -> str:
 
         Here's the topic provided by the user for which you need to generate a post:
         <topic>
-        {topic}
+            {topic}
         </topic>
+
+        Here are some examples of topics and generated posts:
+        <examples>
+            {examples_str}
+        </examples>
+
+        Please use the tone, language, structure , and style of the examples provided above to generate a post that is engaging and relevant to the topic provided by the user.
+        Don't use the content from the examples!
     """
 
     response = client.responses.create(
